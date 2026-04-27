bad3ec1c51f88d21e5a28a35867293ae692b7854
Davis Miyashiro <davis.miyashiro@gmail.com>
2026-04-27 17:03:46 +0100

    Upgrade dependencies and more cleaning up.

--- app/src/androidTest/java/com/exercise/davismiyashiro/popularmovies/movies/MoviesListTest.kt
+++ app/src/androidTest/java/com/exercise/davismiyashiro/popularmovies/movies/MoviesListTest.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.hasText
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
